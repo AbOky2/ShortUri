6ddc5d1b42f43a195a890b4d26b4ac15a2bbff67
AbOky2 <issaokiabderamane@gmail.com>
2025-03-31 22:46:18 +0200

    Ecriture d'un test qui crée une url courte à partir d'une url originale

---
+++ src/test/java/com/example/shorturi/controller/ShortUrlControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.shorturi.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ShortUrlControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldCreateShortUrl() throws Exception {
+        String originalUrl = "https://www.google.com";
+        mockMvc.perform(post("/shortUrl")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"originalUrl\": \"" + originalUrl + "\"}"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("http://localhost:8080/shortUrl"));
+    }
+
+}
