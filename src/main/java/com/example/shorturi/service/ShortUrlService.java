a8768d0fe4d2e23c1ad2c6d773e8d6e34fd54a6a
AbOky2 <issaokiabderamane@gmail.com>
2025-04-01 00:53:49 +0200

    Ajout d'un log sur le service pour verifier pourquoi j'ai un retour 200 au lieu de 400

--- src/main/java/com/example/shorturi/service/ShortUrlService.java
+++ src/main/java/com/example/shorturi/service/ShortUrlService.java
@@ -17,6 +17,7 @@ public class ShortUrlService {
     }
 
     public String createShortUrl(String originalUrl) {
+        System.out.println("Received originalUrl: '" + originalUrl + "'");
         if(originalUrl == null || originalUrl.trim().isEmpty()) {
             throw new IllegalArgumentException("l'URL ne peut pas être vide");
         }
